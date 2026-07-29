2576fe399728a57b6e5ad544d3168a7e7757e282
Saloni1911R <12345678+Saloni1911R@users.noreply.github.com>
2026-07-29 10:29:33 +0530

    Add Lab 3 files for class experiments

---
+++ class_exp/Lab_3/Marging_2sorted_ARRAY.py
@@ -0,0 +1,26 @@
+def merging(A,B):
+    merge = [0]*(len(A)+len(B))
+    i,j,k = 0,0,0
+    while (i<len(A) and j < len(B)):
+        if(A[i] < B[j]):
+            merge[k] = A[i]
+            i = i+1
+            k = k+1
+        else :
+            merge[k] = B[j]
+            j = j+1
+            k = k+1
+    while(i<len(A)):
+        merge[k] = A[i]
+        i = i+1
+        k = k+1
+    while(i<len(B)):
+            merge[k] = B[j]
+            j = j+1
+            k = k+1
+    return merge
+
+A = [1,3,4,5,6,7,9,15]
+B = [2,8,16,19,56]
+
+print(merging(A,B))
