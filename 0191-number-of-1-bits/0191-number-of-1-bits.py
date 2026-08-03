85067f8b614d1fcce37eb0fb716429222d464f20
Saloni Ramavat <saloniramavat5@gmail.com>
2026-08-03 09:31:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.93%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        
+        while n > 0:
+            # This clears the lowest set bit (turns the rightmost 1 into 0)
+            n = n & (n - 1)
+            count += 1
+            
+        return count
