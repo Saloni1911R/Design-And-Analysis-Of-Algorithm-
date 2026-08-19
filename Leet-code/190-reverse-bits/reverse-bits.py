ead5d0154d3c4c86ed57a3a375b600e234bcf635
Saloni Ramavat <saloniramavat5@gmail.com>
2026-08-19 07:58:07 +0530

    Time: 17 ms (58.58%) | Memory: 12.3 MB (88.67%) - LeetSync

--- Leet-code/190-reverse-bits/reverse-bits.py
+++ Leet-code/190-reverse-bits/reverse-bits.py
@@ -7,13 +7,8 @@ class Solution(object):
         result = 0
         
         for _ in range(32):
-            # Shift result to the left for the next bit
             result <<= 1
-            
-            # Extract the rightmost bit of n and add it to result
             result |= (n & 1)
-            
-            # Shift n to the right to process the next bit
             n >>= 1
             
         return result
