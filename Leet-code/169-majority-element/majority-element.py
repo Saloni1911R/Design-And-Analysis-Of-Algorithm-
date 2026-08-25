4dd9b3ec65ba00141c7a257425550c94ea83b8da
Saloni Ramavat <saloniramavat5@gmail.com>
2026-08-25 09:55:12 +0530

    Time: 71 ms (5.31%) | Memory: 13.5 MB (73.88%) - LeetSync

--- Leet-code/169-majority-element/majority-element.py
+++ Leet-code/169-majority-element/majority-element.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        candidate = None
-        count = 0
+        if len(nums) <= 1:
+            return nums[0]
+        mid = len(nums)//2
+        left_majority = self.majorityElement(nums[:mid])
+        right_majority = self.majorityElement(nums[mid:])
+        if left_majority == right_majority:
+            return left_majority
+
+        left_count = nums.count(left_majority)
+        right_count = nums.count(right_majority)
         
-        for num in nums:
-            if count == 0:
-                candidate = num
-            if num == candidate :
-                count += 1 
-            else :
-                count -= 1 
-            
-        return candidate
+        return left_majority if left_count > right_count else right_majority
