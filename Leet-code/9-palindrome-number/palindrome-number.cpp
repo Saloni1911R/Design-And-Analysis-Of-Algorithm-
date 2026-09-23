c6a6f1fa24af2931d3c38b2ab8a011d6410eeba0
Saloni Ramavat <saloniramavat5@gmail.com>
2026-09-23 16:11:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (35.96%) - LeetSync

--- Leet-code/9-palindrome-number/palindrome-number.cpp
+++ Leet-code/9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x == 0) return true;
-        
-        if(x <  0 || (x % 10 == 0)){
-            return false;
-        }
-        int r = 0;
-        while(x>r){
+        if(x<0) return false;
+        long orig = x;
+        long rev = 0;
+        while(x!=0){
             int d = x % 10;
-            r = r * 10 + d;
-            x /= 10;
+            rev = rev * 10 + d;
+            x = x / 10;
         }
-        return (r == x || x == r /10);
+        if (rev == orig){
+            return true;
+        }
+    return false;
     }
 };
